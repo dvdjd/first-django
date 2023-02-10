5bbd8ad7b703809e5bf63d6f2f19f0a8d7d6616b
unknown <Jude David Acula@LAPTOP-PQOHFF1D.NMCP2021.com>
2023-02-10 16:18:30 +0800

    added login page

--- smdar_root/pages/views.py
+++ smdar_root/pages/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from . models import Page
 
 # Create your views here.
-def index (request) :
+def index (request, pagename) :
     # return HttpResponse("<h1>Hello Django Python</h1>")
-    return render(request, 'pages/page.html')
+    # return render(request, 'pages/page.html')
+    
+	pagename = '/' + pagename
+	pg = Page.objects.get(permalink=pagename)
+	context = {
+		'title': pg.title,
+		'content': pg.bodytext,
+		'last_updated': pg.update_date
+	}
+	# assert False (set to True to test the view)
+	return render(request, 'pages/page.html', context)
+
+def login (request) :
+    # return HttpResponse("<h1>Hello Django Python</h1>")
+    return render(request, 'pages/login.html')
